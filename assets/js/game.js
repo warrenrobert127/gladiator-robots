156f066c8e551e445e00a1eb1caa888088e3efff
Robert Warren <2110981@wholefoods.com>
2024-06-09 13:42:29 -0500

    store fighter name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,19 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+
+function fight() {
+    console.log("The fight has begun!");
+
+    console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+    var playerName = window.prompt("What is your robots name?");
+
+    // Note the lack of quotation marks around playerName
+    console.log(playerName);
+
+      
+}
+ fight();
